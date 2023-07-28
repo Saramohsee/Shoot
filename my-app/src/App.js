c3b731d1f6fe5f161244980e221283da80c62b41
Sara Mohsen <sara.mohsen@education.nsw.gov.au>
2023-07-28 15:40:43 +1000

    react application commit

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="opening">
+       <div className='mainDiv'>
+        <h1>remembering HTML</h1>
+        <p>david attengrough has influenced thousands and everyone loves him as he is changing the planet for the better and fights for climate change</p>
+        <a>hello</a>
+        
+
+       </div>
+      </header>
+      {
+       
+        }
+      }
+    
+
+  </div>
+  );
+}
+
+export default App;
