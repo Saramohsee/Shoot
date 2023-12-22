70110964545ed5afd06d2dc5815aa9ed4f5ca6fa
Sara_Mohsen <sara.mohsen@education.nsw.gov.au>
2023-12-23 09:17:14 +1100

    Fixed Background

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -4,7 +4,7 @@ import Form from '../Components/Form';
 export default function Home () {
 
     return (
-        <div>
+        <div className='home'>
             <Form>
         
             </Form>
