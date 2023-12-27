bb2c250f251104a6f09b2a793d2495ec5140a5a6
Saramohsee <136786744+Saramohsee@users.noreply.github.com>
2023-12-27 12:10:06 +1100

    deleted heroku

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Footer from './Components/Footer';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './Pages/Home';
+import Home from './Pages/Home'
 import Form from './Components/Form';
 
 
-heroku 
+ 
 function App() {
 
  return (
