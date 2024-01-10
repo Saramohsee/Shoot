067b8ed67c0ccac9cd6e5453b9226bcbeb80e3dd
Saramohsee <136786744+Saramohsee@users.noreply.github.com>
2024-01-11 08:47:19 +1100

    removed uwu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Footer from './Components/Footer';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Pages/Home'
-import Form from './Components/Form';
+
 
 
  
