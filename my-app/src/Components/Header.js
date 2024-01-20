5aaa212e4066edf680914bd773aea796e897082d
Saramohsee <136786744+Saramohsee@users.noreply.github.com>
2024-01-21 08:02:14 +1100

    help #2

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -5,19 +5,20 @@ export default function Header () {
     return (
       
            <header className={Style.header}>
-            
+            <div className={Style.headerElements}>
                     <img 
                     className={Style.logo}
                     src={require("../Images/YG - logo.png")} 
                     alt='Logo'
                     />
            
-                    <ul>
+                    <ul className={Style.headerButtons}>
                         <li className={Style.quizNav}>Quiz</li>
                         <li className={Style.blogNav}>Blog</li>
                         <li className={Style.glowingProducts}>Glowing Products</li>
 
                     </ul>
+            </div>
 
                    
                   
