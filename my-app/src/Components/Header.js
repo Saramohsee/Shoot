41da36e1bbc624043d70b53708cd275d4902718f
Saramohsee <136786744+Saramohsee@users.noreply.github.com>
2024-01-23 09:12:46 +1100

    ..

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,21 +1,24 @@
 import React from "react";
 import Style from "./Header.module.css"
+import {Link} from 'react-router-dom'
 
 export default function Header () {
     return (
       
            <header className={Style.header}>
             <div className={Style.headerElements}>
+                <Link to='/' >
                     <img 
                     className={Style.logo}
                     src={require("../Images/YG - logo.png")} 
                     alt='Logo'
                     />
+                    </Link>
            
                     <ul className={Style.headerButtons}>
-                        <li className={Style.quizNav}>Quiz</li>
-                        <li className={Style.blogNav}>Blog</li>
-                        <li className={Style.glowingProducts}>Glowing Products</li>
+                        <li className={Style.quizNav}><Link to='quiz' style={{ textDecoration: 'none', color: '#50544F' }}>Quiz</Link></li>
+                        <li className={Style.blogNav}><Link to='blog'style={{ textDecoration: 'none', color: '#50544F' }}>Blog</Link></li>
+                        <li className={Style.glowingProducts}><Link to='products'style={{ textDecoration: 'none', color: '#50544F' }}>Glowing Products</Link></li>
 
                     </ul>
             </div>
--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -10,7 +10,15 @@ export default function Home () {
 
     return (
         <div className={Style.home}>
-            
+            <img 
+            className={Style.eyePic}
+            url='../images/eye.jpg'
+            alt='eye'
+            />
+            <div className={Style.collegen}>
+            <h1>COLLOGEN</h1>
+            <h2>your glowing skin solution</h2>
+            </div>
             
           
            
