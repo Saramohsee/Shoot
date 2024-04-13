b143379bde97091f3443328faebd86fe17513553
Saramohsee <sara.mohsen@education.nsw.gov.au>
2024-04-13 22:49:04 +1000

    uubxs

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -19,7 +19,7 @@ export default function Home () {
             <div className={Style.collegen}>
             <h1>COLLOGEN</h1>
             <h2>your glowing skin solution</h2>
-            <button>Shop Now --> </button>
+            <button>Shop Now -- </button>
             </div>
             
           
--- my-app/src/Pages/Products.js
+++ my-app/src/Pages/Products.js
@@ -107,7 +107,6 @@ export default function Products() {
         </div>
       )}
 
-
     </div>
     )
 }
